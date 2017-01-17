509ca8823580e52f7d4b701e896bee617b66c36f
Damien Reimert <damien.reimert@gmail.com>
2017-01-17 14:42:06 +0100

    Réécrire le code précédent pour créer une fonction qui récupère et affiche un page staskexchange

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const request = require('request');
 
-request('https://github.com/dreimert/cours-javascript-td4/blob/data/data.json?raw=true', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(JSON.parse(body).items) // Show the HTML for the Google homepage.
-  }
-})
+const url = 'https://github.com/dreimert/cours-javascript-td4/blob/data/data.json?raw=true';
+
+const getUrlContentAndShow = function(url) {
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(JSON.parse(body).items) // Show the HTML for the Google homepage.
+    }
+  })
+};
+
+getUrlContentAndShow(url);
