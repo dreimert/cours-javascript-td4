01f6eb56c7e4f9127cd100f36ddbcff6344d644f
Damien Reimert <damien.reimert@gmail.com>
2017-01-17 16:31:27 +0100

    Réécrivez votre code pour qu'il ressemble à celà

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
-const request = require('request');
+const rp = require('request-promise');
 
 const url = 'https://github.com/dreimert/cours-javascript-td4/blob/data/data.json?raw=true';
 
 const getUrlContentParseJson = function(url, callback) {
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      callback(null, JSON.parse(body)); // Show the HTML for the Google homepage.
-    } else {
-      callback(error);
-    }
-  })
+  return rp(url)
+    .then(JSON.parse);
 };
 
 const extractInfos = function(content) {
-  console.log(content.items.map(function(question) {
+  return content.items.map(function(question) {
       return {
         title: question.title,
         tags: question.tags,
         owner: question.owner
       };
     }
-  ));
+  );
 };
 
-getUrlContentParseJson(url, function(err, content){
-  if(!err) {
-    extractInfos(content);
-    const answersUrl = `https://github.com/dreimert/cours-javascript-td4/blob/data/${content.items[0].question_id}.json?raw=true`
-    getUrlContentParseJson(answersUrl, console.log);
-
-  } else {
-    console.error(err);
-  }
-});
+getUrlContentParseJson(url)
+.then(extractInfos)
+.then(console.log)
