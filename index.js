25fb7ce59e297783bb1c157e4b0161dd4044ceb6
Damien Reimert <damien.reimert@gmail.com>
2017-01-17 14:31:39 +0100

    Essayez d'afficher les données renvoyer.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-request('http://www.google.fr', function (error, response, body) {
+request('https://github.com/dreimert/cours-javascript-td4/blob/data/data.json?raw=true', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body) // Show the HTML for the Google homepage.
   }
