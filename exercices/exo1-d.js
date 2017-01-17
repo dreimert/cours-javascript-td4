3c5800e08c204da61b416e7b94a298eebc13c7ac
Damien Reimert <damien.reimert@gmail.com>
2017-01-17 14:03:11 +0100

    Exercice 1d

--- exercices/exo1-d.js
+++ exercices/exo1-d.js
@@ -2,5 +2,5 @@
   La fonction doit appliquer à tous les éléments du tableau en paramètre la fonction `toApply` et retourner un nouveau tableau.
 */
 module.exports = function exo1(tableau, toApply) {
-  return tableau.map(/* START MOD */    /* END MOD */);
+  return tableau.map(/* START MOD */  toApply  /* END MOD */);
 }
